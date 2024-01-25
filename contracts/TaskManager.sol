797b3b6da65aefb37aaac3a8a0ccda6a9ac62614
nbr-23 <the-nbr23@protonmail.com>
2024-01-25 22:31:10 +0100

    + Task structure  update

--- contracts/TaskManager.sol
+++ contracts/TaskManager.sol
@@ -3,5 +3,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract TaskManager {
-    
+
+     enum Priority { LOW, MEDIUM, HIGH }
+     struct Task {
+        uint id;
+        Priority priority;
+        string content;
+        uint timestamp;
+        bool completed;
+    }
 }
