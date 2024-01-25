b8176818e7b290004f65cb7bc145b9c36d870796
nbr-23 <the-nbr23@protonmail.com>
2024-01-25 22:38:28 +0100

    + constructor & variables

--- contracts/TaskManager.sol
+++ contracts/TaskManager.sol
@@ -4,12 +4,23 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract TaskManager {
 
-     enum Priority { LOW, MEDIUM, HIGH }
+     enum Priority { LOW, MEDIUM, HIGH } 
+
      struct Task {
-        uint id;
-        Priority priority;
-        string content;
-        uint timestamp;
-        bool completed;
+        uint id; // Task ID
+        Priority priority; // Task Priority
+        string content; // Task content
+        uint timestamp; // Task creation time
+        bool completed; // Task completed or not state
     }
+
+    address admin;
+    uint private count;
+
+    constructor(){
+        admin = msg.sender;
+        count = 0;
+    }
+
+    
 }
